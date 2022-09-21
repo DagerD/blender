298bc32ba5c76d40079387c5e4b77b0729412f54
markyandex@gmail.com <markyandex@gmail.com>
2022-09-21 12:28:13 +0300

    added DMATERIALX_PYTHON_VERSION and removed renaming for debug configuration

--- build_files/build_environment/cmake/materialx.cmake
+++ build_files/build_environment/cmake/materialx.cmake
@@ -4,6 +4,7 @@ set(MATERIALX_EXTRA_ARGS
   -DMATERIALX_BUILD_PYTHON=ON
   -DMATERIALX_INSTALL_PYTHON=OFF
   -DMATERIALX_PYTHON_EXECUTABLE=${PYTHON_BINARY}
+  -DMATERIALX_PYTHON_VERSION=${PYTHON_SHORT_VERSION}
 )
 
 if(WIN32)
@@ -24,20 +25,4 @@ if(WIN32)
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${LIBDIR}/materialx/python/ ${HARVEST_TARGET}/materialx/python/${BUILD_MODE}
     DEPENDEES install
   )
-
-  if(BUILD_MODE STREQUAL Debug)
-    add_custom_command(TARGET external_materialx POST_BUILD
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXCore_d_d.cp310-win_amd64.pyd PyMaterialXCore_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXFormat_d_d.cp310-win_amd64.pyd PyMaterialXFormat_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXGenGlsl_d_d.cp310-win_amd64.pyd PyMaterialXGenGlsl_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXGenMdl_d_d.cp310-win_amd64.pyd PyMaterialXGenMdl_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXGenOsl_d_d.cp310-win_amd64.pyd PyMaterialXGenOsl_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXGenShader_d_d.cp310-win_amd64.pyd PyMaterialXGenShader_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXRender_d_d.cp310-win_amd64.pyd PyMaterialXRender_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXRenderGlsl_d_d.cp310-win_amd64.pyd PyMaterialXRenderGlsl_d.cp310-win_amd64.pyd
-       COMMAND ${CMAKE_COMMAND} -E rename PyMaterialXRenderOsl_d_d.cp310-win_amd64.pyd PyMaterialXRenderOsl_d.cp310-win_amd64.pyd
-       WORKING_DIRECTORY ${HARVEST_TARGET}/materialx/python/${BUILD_MODE}/MaterialX
-       COMMENT "Renaming files..."
-    )
-  endif()
 endif()
